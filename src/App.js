3cc5d4e46029299a99f53ef211c78648ace54df2
tenadem <tenadem83@gmail.com>
2022-10-13 15:12:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Data from "./components/Data";
 
 function App() {
-  return <div className="App">App js</div>;
+  return (
+    <div className="App">
+      <Data />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/data.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Data = () => {
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    axios
+      .get("/db/data.json")
+      .then((res) => setData(res.data[0].address.values))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  //1-Compute the sum of all numbers in the "address" > "values" key.
+
+  //2-Compute the digit sum of the result in step 1.
+  const sumOfDigits = () => {
+    const sumOfAllNumbers = data.reduce((previousValue, currenValue) => {
+      return previousValue + currenValue;
+    }, 0);
+    let num = sumOfAllNumbers;
+    let sum = 0;
+    while (sumOfAllNumbers > 0) {
+      let rem = num % 10;
+      sum = sum + rem;
+      num = parseInt(num / 10);
+    }
+    return sum;
+  };
+
+  console.log(sumOfDigits());
+
+  return (
+    <div>
+      <h3>Sum of all numbers in the "address" "values" key:</h3>
+      <h3>digit sum of the result </h3>
+    </div>
+  );
+};
+
+export default Data;
